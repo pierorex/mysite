26458135d6effbe3228e1bedfe62cfd69bc2983e
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-22 00:29:48 -0430

    Learned how to customize functional attributes of the admin pages. Starting learning how to use templates.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import utils
 import datetime
 
 class Question(models.Model):
@@ -8,7 +9,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
-    def was_published_recently(self): return self.pub_date >= datetime.timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self): return self.pub_date >= utils.timezone.now() - datetime.timedelta(days=1)
+    
+    was_published_recently.admin_order_field = pub_date
+    was_published_recently.boolean = True
+    was_published_recently.short_desc = 'Published recently?'
     
     
 class Choice(models.Model):

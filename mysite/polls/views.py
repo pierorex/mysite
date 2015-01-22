b664eb985b390e6240dc5ef9c32f7f2e24004a7c
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-22 12:08:46 -0430

    Fixed index view.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Question
 
-# Create your views here.
+
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[5:]
+    latest_question_list = Question.objects.order_by('-pub_date')[2:]
     response = ','.join([p.question_text for p in latest_question_list])
     return HttpResponse(response)
 

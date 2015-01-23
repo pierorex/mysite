c543f7c5f5863c53055c2e884bb7743fc3d7c56a
Jean Piero Hernandez Meze <jeanpierohm@gmail.com>
2015-01-22 23:46:50 -0430

    Deleted comment on admin.py.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-# Register your models here
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
